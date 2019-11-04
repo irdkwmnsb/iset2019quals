cc74ce2a2746ea02d10490ce376d4efc03131fc9
umfc <umfc@niuitmo.ru>
2019-11-04 07:09:24 +0300

    ready to khe khe

---
+++ tasks/meme-web-150/server/app/static/rzhaka.js
@@ -0,0 +1,11 @@
+let anime = document.getElementById('container'),
+    slide = 0,
+    mult = 10;
+
+setInterval(function(){
+    slide += mult;
+    if(slide % 100 == 0){
+        mult = -mult
+    }
+    anime.style = 'filter: invert(' + slide + '%);';
+}, 12);
