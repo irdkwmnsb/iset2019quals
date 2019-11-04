bbee654a8c246a4e0cc3345f6ca478703718debe
umfc <umfc@niuitmo.ru>
2019-11-04 22:32:53 +0300

    title dobavil
    a

--- tasks/admin-web-50/server/app/index.php
+++ tasks/admin-web-50/server/app/index.php
@@ -20,6 +20,7 @@
 	}
 ?>
 <head>
+<title>Admin Panel</title>
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <style>
         body {
