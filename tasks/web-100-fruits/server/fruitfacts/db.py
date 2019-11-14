3b964f72a9a24a34fd19c33e38a20a7801ca4916
Maxim <supermax74.02@gmail.com>
2019-11-15 00:24:09 +0300

    ✋web-100-fruits race condition fix via mongoimport
    Ну короче когда запускаешь populate в 4х воркерах gunicorn-а может случиться такое, что база данных будет наполнена дважды. Чтобы этого избежать используем mongoimport

--- tasks/web-100-fruits/server/fruitfacts/db.py
+++ tasks/web-100-fruits/server/fruitfacts/db.py
@@ -4,6 +4,3 @@ from fruitfacts import config
 
 client = pymongo.MongoClient(config.MONGO_URL)
 db = client[config.MONGO_DB]
-
-from fruitfacts.populate import check_and_populate
-check_and_populate()
